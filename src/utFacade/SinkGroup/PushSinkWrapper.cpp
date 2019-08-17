9d803620083d224d8e419e78c38485b2e5bb8bc3
Ulrich Eck <ueck@net-labs.de>
2019-08-17 16:32:31 +0200

    add template implementations for pushsinkwrapper needed on windows

---
+++ src/utFacade/SinkGroup/PushSinkWrapper.cpp
@@ -0,0 +1,27 @@
+#include "utFacade/SinkGroup/PushSinkWrapper.h"
+           
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::Button  >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::Distance >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::Position2D >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::Position >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::Vector4D >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::Vector8D >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::Rotation >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::Matrix3x3 >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::Matrix3x4 >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::Matrix4x4 >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::Pose >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::ErrorPose >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::ErrorPosition2 >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::ErrorPosition >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::RotationVelocity >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::CameraIntrinsics >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::ButtonList >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::DistanceList >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::IDList >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::PoseList >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::PositionList2 >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::PositionList >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::ErrorPoseList >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::ErrorPositionList2 >;
+template class Ubitrack::Facade::PushSinkWrapper  < Ubitrack::Measurement::ErrorPositionList >;
